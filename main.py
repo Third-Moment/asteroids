f8c3e1faa9b5ef65ae91d93793abeecfc9a8fa74
Third-Moment <Third-Moment@users.noreply.github.com>
2025-04-22 03:03:19 +0000

    add shooting

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from player import Shot
+
 
 def main():
     print("Starting Asteroids!")
@@ -16,7 +18,8 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
@@ -25,6 +28,8 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
+    Shot.containers = (shots, updatable, drawable)
 
     dt = 0
 
